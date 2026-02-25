b7eafd073d8a03e493fd01b83e29a11f530eee76
Som Gupta <somgupta0011@gmail.com>
2026-02-25 19:56:53 +0530

    updated classes

---
+++ Spring-Boot-CRUD-Soft-Delete/src/main/java/com/boot/mvc/FullStackProjectBootMvcCrudApplicationSoftDelete.java
@@ -0,0 +1,13 @@
+package com.boot.mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullStackProjectBootMvcCrudApplicationSoftDelete {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullStackProjectBootMvcCrudApplicationSoftDelete.class, args);
+	}
+
+}
